86654e1327258494961c503e7f84b1fc5d73c985
Michel Peterson <michel@peterson.com.ar>
2009-02-02 09:30:41 -0200

    Enabled Index for static pages.
    This is very useful for debugging.

--- urls.py
+++ urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.BASE_DIR+'/core/static/', 'show_indexes': False}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.BASE_DIR+'/core/static/', 'show_indexes': True}),
 )
